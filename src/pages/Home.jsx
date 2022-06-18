54ab11d8a58567fdb63ab96da9e46bffb50130d0
AashimaKatoch <aashimasaina@gmail.com>
2022-06-18 19:31:56 +0530

    added radio button and added search feauture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,125 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config';
+
 
 const Home = () => {
-  return (
-    <MainPageLayout>This is Home</MainPageLayout>
-  )
+const [input, setInput] =useState('');
+const [results,setResults] =useState(null);
+const [searchOptions , setSearchOptions] = useState('shows');
+
+const isShowSearch = searchOptions ==='shows';
+
+
+const onSearch = () =>{
+  // https://api.tvmaze.com/search/shows?q=men
+
+  apiGet(`/search/${searchOptions}?q=${input}`).then(result=>{
+
+   setResults(result);
+    
+  });
+
 }
+
+const onInputChange = (ev)=>{
+
+  setInput(ev.target.value);
+
+}
+
+
+
+const onKeyDown = ev =>{
+ if(ev.key === 'Enter'){
+  onSearch();
+
+ }
+
+};
+
+
+const onRadioChange=(ev)=>{
+
+  setSearchOptions(ev.target.value);
+};
+
+
+
+
+const renderResults =()=>
+{
+  if(results && results.length===0)
+  {
+
+    return <div>No results</div>
+
+  }
+
+  if(results && results.length > 0)
+  { return results[0].show ? results.map(item=>(
+    <div 
+    key ={item.show.id}>{item.show.name}
+    </div>)) : results.map(item=>(
+           <div 
+           key ={item.person.id}>{item.person.name}
+           </div>))
+     
+
+  }
+
+
+  return null;
+};
+
+
+
+
+return (
+    <MainPageLayout>
+      <input type="text"
+      placeholder='Search for something'
+       onChange={onInputChange}  
+       onKeyDown={onKeyDown} 
+       value={input}/>
+
+
+<div>
+
+<label htmlFor='shows-search'>
+  Shows
+  <input id = "shows-search " 
+  type="radio" 
+  value="shows" 
+  checked ={isShowSearch} 
+  onChange={onRadioChange}
+  />
+
+</label>
+
+
+<label htmlFor='actors-search'>
+
+   Actors
+  <input id = "actors-search " 
+  type="radio" 
+  checked ={!isShowSearch} 
+  value="people" 
+  onChange={onRadioChange}/>
+
+</label>
+
+
+</div>
+
+      <button type='button' 
+      onClick={onSearch} >
+      Search
+      </button>
+
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
 
 export default Home
